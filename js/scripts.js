1c6d328fdadf17e32b032c600bb917f5af6e853f
Наймушин Егор <webmaster@farexpo.local>
2019-06-14 16:56:59 +0300

    vers1406

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	
+	//new WOW().init();
+	
+});
+
+/*$(window).load(function(){
+	
+	$('.sup-items').slick({
+	  infinite: true,
+	  autoplay: true,
+	  speed: 1500,
+	  slidesToShow: 4,
+	  slidesToScroll: 1,
+	  responsive: [
+		{
+		  breakpoint: 1199,
+		  settings: {
+			slidesToShow: 3,
+		  }
+		},
+		{
+		  breakpoint: 991,
+		  settings: {
+			slidesToShow: 2,
+		  }
+		},
+		{
+		  breakpoint: 680,
+		  settings: {
+			slidesToShow: 1,
+		  }
+		}
+	  ] 
+	});
+	
+});*/
