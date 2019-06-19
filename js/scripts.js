e0ca69fab8dd822fca9993fd20bfed54d93adcec
Наймушин Егор <webmaster@farexpo.local>
2019-06-19 18:10:48 +0300

    vers 1906

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,28 @@
 $(document).ready(function(){
 	
-	//new WOW().init();
+	new WOW().init();
 	
+	//comments: more - less
+	$('.all').click(function(){
+		$(this).siblings('.comment-text').toggleClass('more-text');
+		$(this).children('.glyphicon').toggleClass('rotate');
+		let text = $(this).children('.more-less').text();
+		if(text == 'Читать полностью') {
+			$(this).children('.more-less').text('Свернуть');
+		}
+		else {
+			$(this).children('.more-less').text('Читать полностью');
+		}
+	});
+	
+	$('.close-btn').click(function(){
+		$('.modal-window').css('display','none');
+		$('body').css('overflow','visible');
+	})
+	$('.yellow-btn').not('.modal-btn').click(function(){
+		$('.modal-window').css('display','flex');
+		$('body').css('overflow','hidden');
+	})
 });
 
 /*$(window).load(function(){
